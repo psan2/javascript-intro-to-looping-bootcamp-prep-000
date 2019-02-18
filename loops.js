e8330e757411e405b6ed84da688bdbb12b6096e1
Patrick Santucci <patrick.s.santucci@gmail.com>
2019-02-18 22:26:21 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,9 +1,18 @@
 function forLoop(arr){
-   for (var i = 0; i < 25; i++) {
-     if (i === 1) {
-       arr.push("I am 1 strange loop.")
-     }
-     else arr.push(`I am ${i} strange loops.`)
-   }
-   return arr
- }
+  for (var i = 0; i < 25; i++) {
+    if (i === 1) {
+      arr.push("I am 1 strange loop.")
+    }
+    else arr.push(`I am ${i} strange loops.`)
+  }
+  return arr
+};
+
+function whileLoop(n){
+  while n > 0 {
+    console.log(--n)
+  }
+  return "done"
+}
+
+
