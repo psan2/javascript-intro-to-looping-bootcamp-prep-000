19e674c6aace440771deadbb021889fff1868ee8
Patrick Santucci <patrick.s.santucci@gmail.com>
2019-02-18 22:38:10 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -14,3 +14,14 @@ function whileLoop(countdown){
   }
   return ("done");
 }
+
+function incrementVariable() {
+  i = i + 1;
+  return i;
+}
+
+function doWhileLoop(num){
+  do {
+    console.log("I run once regardless.")
+  } while (incrementVariable() < num)
+}
