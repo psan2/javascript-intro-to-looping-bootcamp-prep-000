db3fd537416c6cc48b4760f412139ad6df74cb26
Patrick Santucci <patrick.s.santucci@gmail.com>
2019-02-18 22:18:25 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,3 +1,8 @@
  function forLoop(arr){
-
+   for (var i = 0; i < 25; i++) {
+     if (i === 0) {
+       arr.push("I am 1 strange loop.")
+     }
+     else arr.push("I am ${i} strange loops.")
+   }
  }
